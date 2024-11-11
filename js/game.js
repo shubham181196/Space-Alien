25c0939dba9ecf69465cd0ee6ada5cc26efd1dcb
shubham181196 <87092826+shubham181196@users.noreply.github.com>
2024-11-11 12:46:57 +0530

    update game.js

---
+++ js/game.js
@@ -0,0 +1,9 @@
+// refrencing to gloabal phasor
+const config={
+    type: Phasor.AUTO,
+    width:1920,
+    height:1080,
+    backgroundColor:0x5f6e7a
+}
+const game=new Phasor.Game(config);
+console.log(game);
