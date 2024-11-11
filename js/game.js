2e08d0425afbb14eb29c0c3e7ea03942d8863502
shubham181196 <87092826+shubham181196@users.noreply.github.com>
2024-11-11 13:47:18 +0530

    updated game.js

--- js/game.js
+++ js/game.js
@@ -1,9 +1,21 @@
 // refrencing to gloabal phasor
+
+// game scene
 const config={
-    type: Phasor.AUTO,
+    type: Phaser.AUTO,
     width:1920,
     height:1080,
-    backgroundColor:0x5f6e7a
+    physics:{
+        default:'arcade',
+        arcade:{
+            debug:true
+        }
+    },
+    backgroundColor:0x5f6e7a,
+    scale:{
+        mode:Phaser.Scale.FIT,
+        autoCenter:Phaser.Scale.CENTER_BOTH
+    }
 }
-const game=new Phasor.Game(config);
+const game=new Phaser.Game(config);
 console.log(game);
